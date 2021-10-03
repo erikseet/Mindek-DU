00c2fd77c9c4f751681b99c3b43e0acabc88670d
erikseet <51919771+erikseet@users.noreply.github.com>
2021-10-03 23:58:15 +0200

    Add files via upload

---
+++ Vajcovka max.py
@@ -0,0 +1,23 @@
+
+def cislo():
+    while True:
+        cislo = input("Zadaj počet súborov: ")
+        try:
+            return int(cislo)
+        except ValueError:
+            print("Nebolo zadané správne číslo")
+
+cislo = cislo()
+input = []
+pocet = 0
+
+with open('basnicka.txt', encoding='utf-8') as subor:
+    for slovo in subor:
+        input += slovo.split()
+
+for i in range(cislo):
+    pocet += 1
+    if pocet == len(input):
+        pocet -= len(input)
+    with open(f"""slovo{pocet}""", mode="w", encoding="utf-8") as subor:
+        print(input[pocet - 1], file=subor)
