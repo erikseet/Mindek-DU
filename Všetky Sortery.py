22fc014058429b03c86c06b2837eede25181be2d
erikseet <51919771+erikseet@users.noreply.github.com>
2022-02-20 23:24:09 +0100

    Update and rename Všetky Naraz.py to Všetky Sortery.py

--- Všetky Sortery.py
+++ Všetky Sortery.py
@@ -1,13 +1,19 @@
 import random
 from timeit import default_timer as timer
 
-cas = timer()
-arr = []
-for i in range(0,5000):
-    if random.randint(0,1):
-        arr.append(i)
-        random.shuffle(arr)
+arr = random.sample(range(5000), 5000)
 
+print("Array ktory chceme zoradit: ",arr)
+
+arr_bubble = arr
+arr_quicksort = arr
+arr_merge = arr
+arr_selection = arr
+arr_insertion = arr
+
+
+
+start_quicksort = timer()
 def partition(arr, low, high):
     i = (low - 1)
     pivot = arr[high]
@@ -23,21 +29,19 @@ def partition(arr, low, high):
 def quickSort(arr, low, high):
     if len(arr) == 1:
         return arr
-
     if low < high:
         pi = partition(arr, low, high)
         quickSort(arr, low, pi - 1)
         quickSort(arr, pi + 1, high)
-end = timer()
-
-print("Array ktory chceme zoradit: ",arr)
 n = len(arr)
 quickSort(arr, 0, n - 1)
-print("Zoradeny array QuickSortu: ",arr)
-print("Zoradenie pomocou QuickSortu trvalo: ", end - cas, "sekúnd.")
+end = timer()
+cas_quicksorter = end - start_quicksort
 
 
 
+
+start_bubblesorter = timer()
 def bubbleSort(arr):
     n = len(arr)
     for i in range(n - 1):
@@ -46,24 +50,12 @@ def bubbleSort(arr):
                 arr[j], arr[j + 1] = arr[j + 1], arr[j]
 
 bubbleSort(arr)
-end1 = timer()
-print("Zoradeny array BubbleSortu je: ",arr)
-print("Zoradenie pomocou BubbleSortu trvalo: ", end1 - cas, "sekúnd.")
+end_bubblesorter = timer()
+cas_bubblesorter = end_bubblesorter - start_bubblesorter
 
 
 
-for i in range(len(arr)):
-    min_idx = i
-    for j in range(i + 1, len(arr)):
-        if arr[min_idx] > arr[j]:
-            min_idx = j
-    arr[i], arr[min_idx] = arr[min_idx], arr[i]
-
-end2 = timer()
-print("Zoradeny array BubbleSortu je: ",arr)
-print("Zoradenie pomocou BubbleSortu trvalo: ", end2 - cas, "sekúnd.")
-
-
+start_insertionsorter = timer()
 def insertionSort(arr):
     for i in range(1, len(arr)):
 
@@ -73,13 +65,12 @@ def insertionSort(arr):
             arr[j + 1] = arr[j]
             j -= 1
         arr[j + 1] = key
-
 insertionSort(arr)
 end3 = timer()
-print("Zoradeny array InsertionSortu je: ",arr)
-print("Zoradenie pomocou InsertionSortu trvalo: ", end3 - cas, "sekúnd.")
+cas_insertionsort = end3 - start_insertionsorter
 
 
+start_mergesorter = timer()
 def mergeSort(arr):
     if len(arr) > 1:
         mid = len(arr) // 2
@@ -111,9 +102,24 @@ def mergeSort(arr):
 if __name__ == '__main__':
     mergeSort(arr)
     end4 = timer()
-    print("Zoradeny array MergeSortu je: ",arr)
-    print("Zoradenie pomocou MergeSortu trvalo: ", end4 - cas, "sekúnd.")
+    cas_mergesorter = end4 - start_mergesorter
 
-lol = [end - cas,end1 - cas,end2 - cas,end3 - cas,end4 - cas]
-lol.sort(reverse=True)
-print("Zoradene od najvačšieho:", lol)
+start_selectionsort = timer()
+for i in range(len(arr)):
+    min_idx = i
+    for j in range(i + 1, len(arr)):
+        if arr[min_idx] > arr[j]:
+            min_idx = j
+    arr[i], arr[min_idx] = arr[min_idx], arr[i]
+
+end5 = timer()
+cas_selectionsorter = end5 - start_selectionsort
+
+
+
+print("Zoradené pole je: ", arr_bubble, "sek.")
+print("Rýchlosť quick sorteru je: ",cas_quicksorter, "sekund.")
+print("Rýchlosť insertion sorteru je: ",cas_insertionsort, "sekund.")
+print("Rýchlosť merge sorteru je: ",cas_mergesorter, "sekund.")
+print("Rýchlosť selection sorteru je: ",cas_selectionsorter, "sekund.")
+print("Rýchlosť bubble sorteru je: ",cas_bubblesorter, "sekund.")
